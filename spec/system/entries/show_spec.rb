cbd7ccd4bef7aa6162afbc29024e2d915dd79038
Jeremy Albin <thefirstjeremy@gmail.com>
2023-04-25 11:14:50 -0500

    Small test scaffolding for entry show page

---
+++ spec/system/entries/show_spec.rb
@@ -0,0 +1,81 @@
+RSpec.describe "Showing an entry" do
+    before do
+        # Create a user and sign it in
+    end
+    context "without any content" do
+        it "will show an empty content message" do
+            pending "Test can be written"
+            fail
+        end
+    end
+    context "with content" do
+        it "will show the content" do
+            pending "Test can be written"
+            fail
+        end
+    end
+    context "without a title" do
+        it "will show an empty title message" do
+            pending "Test can be written"
+            fail
+        end
+    end
+    context "with a title" do
+        it "will show the title" do
+            pending "Test can be written"
+            fail
+        end
+    end
+    context "without any pictures" do
+        it "will show an empty pictures message" do
+            pending "Test can be written"
+            fail
+        end
+    end
+    context "with a picture" do
+        it "will show the picture" do
+            pending "Test can be written"
+            fail
+        end
+        context "when hovering over the picture" do
+            it "will show the picture's description" do
+                pending "Test can be written"
+                fail
+            end
+            it "will show the picture's title" do
+                pending "Test can be written"
+                fail
+            end
+        end
+    end
+    context "without any lessons" do
+        it "will show an empty lessons message" do
+            pending "Test can be written"
+            fail
+        end
+    end
+    context "with a lesson" do
+        it "will show the lesson" do
+            pending "Test can be written"
+            fail
+        end
+        context "that is taught by someone" do
+            it "will show the lesson's taught date and the teacher" do
+                pending "Test can be written"
+                fail
+            end
+        end
+        context "that is not taught by someone" do
+            it "will show the lesson's learned date" do
+                pending "Test can be written"
+                fail
+            end
+        end
+    end
+    context "without any milestones" do
+        it "will show an empty milestones message" do
+            pending "Test can be written"
+            fail
+        end
+    end
+end
